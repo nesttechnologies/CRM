5ad40949f0524f6d3c1ffd6754f6ebf183bd9d55
Ganesh Krishnan Maha <mahaganesh23@users.noreply.github.com>
2017-08-03 15:52:53 -0400

    Add files via upload

--- Cyber_Revenue_Model/Cyber_Revenue_Model/Program.cs
+++ Cyber_Revenue_Model/Cyber_Revenue_Model/Program.cs
@@ -13,17 +13,30 @@ namespace Cyber_Revenue_Model
     {
         static void Main(string[] args)
         {
-            string URL = "https://api.intrinio.com/financials/reported?identifier=GOOGL&statement=income_statement&&fiscal_year=2016&fiscal_period=Q3&API_USERNAME:API_PASSWORD";
+
+            string URL = "https://api.intrinio.com/financials/reported"+
+                "?identifier=GOOGL"+
+                "&statement=income_statement"+
+                "&&fiscal_year=2016"+
+                "&fiscal_period=Q3" +
+                "&API_USERNAME:API_PASSWORD";
             
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
             request.ContentType = "application/json; charset=utf-8";
             request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("701838466ee4639736a4db8e8dd4f32c:ee5d16f3bc0d4b3f3f7cea2f4069699e"));
             request.PreAuthenticate = true;
             HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            var jsonres = response.GetResponseStream().ToString();
             using (Stream responseStream = response.GetResponseStream())
             {
                 StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                Console.WriteLine(reader.ReadToEnd());
+                // Console.WriteLine(reader.ReadToEnd());
+                /*Console.WriteLine(reader.ReadToEnd());
+                Console.ReadKey();
+                */
+                Financial_Data data = JsonConvert.DeserializeObject<Financial_Data>(reader.ReadToEnd());
+               // var revenue = data.data;
+                Console.WriteLine(data.data[0].value);
                 Console.ReadKey();
             }
         }
@@ -44,7 +57,7 @@ namespace Cyber_Revenue_Model
     {
         public string xbrl_tag { get; set; }
         public string domain_tag { get; set; }
-        public float value { get; set; }
+        public string value { get; set; }
     }
 
 }
