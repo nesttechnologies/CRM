5d4e348007b55659320616cbc5761c198ca8ef9d
Ganesh Krishnan Maha <mahaganesh23@users.noreply.github.com>
2017-08-03 13:53:46 -0400

    Add files via upload

---
+++ Cyber_Revenue_Model/Cyber_Revenue_Model/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cyber_Revenue_Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://api.intrinio.com/financials/reported?identifier=GOOGL&statement=income_statement&&fiscal_year=2016&fiscal_period=Q3&API_USERNAME:API_PASSWORD";
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.ContentType = "application/json; charset=utf-8";
+            request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("701838466ee4639736a4db8e8dd4f32c:ee5d16f3bc0d4b3f3f7cea2f4069699e"));
+            request.PreAuthenticate = true;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                Console.WriteLine(reader.ReadToEnd());
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Financial_Data
+
+    {
+        public Datum[] data { get; set; }
+        public int result_count { get; set; }
+        public int page_size { get; set; }
+        public int current_page { get; set; }
+        public int total_pages { get; set; }
+        public int api_call_credits { get; set; }
+    }
+
+    public class Datum
+    {
+        public string xbrl_tag { get; set; }
+        public string domain_tag { get; set; }
+        public float value { get; set; }
+    }
+
+}
+
+
+
